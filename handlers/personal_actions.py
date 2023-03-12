b6a1054dc538b2e74fb7aec78af5157e00346592
Balashov Dmitrii <49812139+purplebread-git@users.noreply.github.com>
2023-03-12 18:23:38 +0300

    update (mac)

--- handlers/personal_actions.py
+++ handlers/personal_actions.py
@@ -17,11 +17,11 @@ markup.add(item1, item2, item3)
 
 @dp.message_handler(commands="start")
 async def start(message: types.Message):
-    await message.bot.send_message(message.from_user.id, "Добро пожаловать!", reply_markup=markup)
+    await message.bot.send_message(message.from_user.ida, "Добро пожаловать!", reply_markup=markup)
 
 @dp.message_handler()
 async def message(message: types.Message):
-    global count
+    global count, lines
     msg = message['text']
     print(count)
     if count == 0:
@@ -45,8 +45,17 @@ async def message(message: types.Message):
             lines[i] = lines[i].split(' -', 1)
             lines[i][1] = float(lines[i][1].replace(' ', ''))
             print(lines[i])
-        inp = input().split('\n')
-        print(inp)
-        await message.bot.send_message(message.from_user.id, inp.text, reply_markup=markup)
+
+        print(lines[0][0])
+        await message.bot.send_message(message.from_user.id, '1', reply_markup=markup)
+    elif count == 0:
+        if msg == "Сгенерить ценники":
+            await message.bot.send_message(message.from_user.id, 'Отправьте запрос одним сообщением', reply_markup=markup)
+            count=2
+    elif count == 2:
+        zapros = msg
+        for i in range(0, len(lines)):
+            
+
 
 
