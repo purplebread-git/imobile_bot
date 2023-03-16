d46fffdf080c0877ef2be507fe6780a6fafc6595
Balashov Dmitrii <49812139+purplebread-git@users.noreply.github.com>
2023-03-16 19:12:54 +0300

    update

--- handlers/personal_actions.py
+++ handlers/personal_actions.py
@@ -49,7 +49,7 @@ async def message(message: types.Message):
             price_text = msg
             lines = price_text.split('\n')
             lines = [x for x in lines if x]
-            print(lines)
+            #print(lines)
             for i in range(0, len(lines)):
                 vremen_mas = lines[i]
                 vremen_mas = vremen_mas.split(' ')[0][0] + vremen_mas.split(' ')[0][1]
@@ -60,7 +60,7 @@ async def message(message: types.Message):
                 lines[i][1] = float(lines[i][1].replace(' ', ''))
 
                 lines[i].insert(0, vremen_mas)
-                print(lines[i])
+                #print(lines[i])
             price_1 = lines
 
 # -------------- Загрузка прайса Ромы ----------------
@@ -72,7 +72,7 @@ async def message(message: types.Message):
             lines = price_text.split('\n')
             lines = [x for x in lines if x]
 
-            print(lines)
+            #print(lines)
             for i in range(0, len(lines)):
                 lines[i] = lines[i].split(' ')
                 lines[i] = ' '.join(lines[i]).split()
@@ -91,9 +91,10 @@ async def message(message: types.Message):
                         vremen_mas.append(vremen_str)
                         for l in range(j + 1, len(lines[i])):
                             vremen_mas.append(lines[i][l])
-                        print(vremen_mas)
+                        #print(vremen_mas)
             price_2 = vremen_mas
-
+        print(price_1)
+        print(price_2)
         await message.bot.send_message(message.from_user.id, 'Прайс записан', reply_markup=markup)
 
     if count == 0 and msg == "Сгенерить ценники":
@@ -108,13 +109,13 @@ async def message(message: types.Message):
             for j in range(0, len(zapros)):
                 if zapros[j] == price_1[i][1]:
                     price_mas.append(price_1[i])
-                    print(price_1[i])
         price_mas_text = '\n'.join(' - '.join(map(str, l)) for l in price_mas)
         await message.bot.send_message(message.from_user.id, 'Саша\n'+price_mas_text, reply_markup=markup)
 
         price_mas = []
         for i in range(0, len(price_2)):
             for j in range(0, len(zapros)):
+                print(j)
                 if zapros[j] == price_2[i][1]:
                     price_mas.append(price_2[i])
                     print(price_2[i])
